1d5442e9094ad55b0a58d77fd98582b0ada62596
E. Hannah <echannah631@gmail.com>
2021-03-03 11:23:27 -0800

    added checkmark accessory when selecting a row

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -26,7 +26,19 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
 }
+
 
